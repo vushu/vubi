c626f8433068ea3685bb23fc250af7ad832f10ec
davu <davu@mir-robots.com>
2022-07-08 08:34:31 +0200

    update sdl_utilities for better errors

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vubi/application.h>
-#define NDEBUG
-int main() { 
+int main() {
     vubi::Application application;
     application.enable_log_messages();
     application.run();
--- src/sdl_utilities.cpp
+++ src/sdl_utilities.cpp
@@ -3,6 +3,8 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
+#include <iostream>
+
 namespace vubi {
 namespace sdl_utilities {
 
@@ -12,7 +14,12 @@ bool initialize_video() {
 
 bool initialize_image() {
     const int flags = IMG_INIT_PNG | IMG_INIT_JPG;
-    return (IMG_Init(flags) & flags) == flags;
+    if ((IMG_Init(flags) & flags) != flags) {
+        std::cout << "IMG_Init: Failed to initialize png and jpg support" << std::endl;
+        std::cout << "IMG_Init: " << IMG_GetError() << std::endl;
+        return false;
+    }
+    return true;
 }
 
 }  // namespace sdl_utilities
